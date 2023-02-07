15a702d9ce92faa46fa2c513e7de9b4dc3dc630a
丁谦 <qian.ding@ycgame.com>
2023-02-07 09:30:14 +0800

    删除废弃函数

--- src/utils.rs
+++ src/utils.rs
@@ -5,26 +5,6 @@ use calamine::{open_workbook_auto, Reader, DataType, Range};
 
 use super::error;
 
-pub fn open_excel(path: &String, tab: &str) -> Result<Range<DataType>> {
-    println!("open_excel {:?} sheet {}", path, tab);
-    let mut workbook= open_workbook_auto(path)?;
-    
-    let range = workbook.worksheet_range(tab)
-        .ok_or(error::AppError::SheetNotFound(tab.to_string()))??;
-    
-    return Ok(range);
-}
-
-pub fn open_excel2(path: &PathBuf, tab: &str) -> Result<Range<DataType>> {
-    println!("open_excel {:?} sheet {}", path, tab);
-    let mut workbook= open_workbook_auto(path)?;
-    
-    let range = workbook.worksheet_range(tab)
-        .ok_or(error::AppError::SheetNotFound(tab.to_string()))??;
-    
-    return Ok(range);
-}
-
 pub fn get_cell(range: &Range<DataType>, x: u32, y: u32) -> String {
     let one = range.get_value((x, y));
     if one.is_none() {
